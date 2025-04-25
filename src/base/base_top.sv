18cd3fabb48783c2171d1cd5d9f196e15294f790
qaztronic <qaztronic@gmail.com>
2025-04-25 15:48:02 -0700

    -

--- src/base/base_top.sv
+++ src/base/base_top.sv
@@ -22,8 +22,20 @@ module base_top
   base_cpu_w cpu_i(.*);
 
   // --------------------------------------------------------------------------------
+  axi4_if #(A,N) axi4_m[2](.*);
+
+  axi4_lite_fanout #(A, N, 'h1000)
+   axi4_lite_fanout_i (.axi4_s(axi4_cpu), .*);
+
+  // --------------------------------------------------------------------------------
+  axi4_lite_register_if #(N, 3) r_if();
+
+  axi4_lite_register_file #(A, N)
+    axi4_lite_register_file_i(.axi4_s(axi4_m[0]), .*);
+
+  // --------------------------------------------------------------------------------
   axi4_lite_default_slave #(A, N)
-    axi4_lite_default_slave(.axi4_s(axi4_cpu), .*);
+    axi4_lite_default_slave(.axi4_s(axi4_m[1]), .*);
 
   // --------------------------------------------------------------------------------
   reg [25:0] counter;
@@ -38,6 +50,10 @@ module base_top
   assign HP_GPIO_RGB2_B = 0;
   assign HP_GPIO_RGB2_G = 0;
   assign HP_GPIO_RGB2_R = 0;
+
+  // --------------------------------------------------------------------------------
+  assign r_if.register_in[14: 0] = '{default: '0};
+  assign r_if.register_in[   15] = 'hb16b00b5;
 
 // --------------------------------------------------------------------------------
 endmodule
